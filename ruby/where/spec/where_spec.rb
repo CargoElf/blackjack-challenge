950de8eb27bdecb84b8a41607cd72b4ab8d1be85
CargoElf <benjamin.m.vansickle@gmail.com>
2020-06-20 10:17:30 -0500

    all base tests passing

--- ruby/where/spec/where_spec.rb
+++ ruby/where/spec/where_spec.rb
@@ -2,31 +2,35 @@ require_relative '../where'
 
 RSpec.describe ArrayExtension::Where do
 
-  let(:boris)   { {:name => 'Boris The Blade', :quote => "Heavy is good. Heavy is reliable. If it doesn't work you can always hit them.", :title => 'Snatch', :rank => 4} }
-  let(:charles) { {:name => 'Charles De Mar', :quote => 'Go that way, really fast. If something gets in your way, turn.', :title => 'Better Off Dead', :rank => 3}}
-  let(:wolf)    { {:name => 'The Wolf', :quote => 'I think fast, I talk fast and I need you guys to act fast if you wanna get out of this', :title => 'Pulp Fiction', :rank => 4} }
-  let(:glen)    { {:name => 'Glengarry Glen Ross', :quote => "Put. That coffee. Down. Coffee is for closers only.",  :title => "Blake", :rank => 5} }
+  let(:boris)   { {name: 'Boris The Blade', quote: "Heavy is good. Heavy is reliable. If it doesn't work you can always hit them.", title: 'Snatch', rank: 4} }
+  let(:charles) { {name: 'Charles De Mar', quote: 'Go that way, really fast. If something gets in your way, turn.', title: 'Better Off Dead', rank: 3}}
+  let(:wolf)    { {name: 'The Wolf', quote: 'I think fast, I talk fast and I need you guys to act fast if you wanna get out of this', title: 'Pulp Fiction', rank: 4} }
+  let(:glen)    { {name: 'Glengarry Glen Ross', quote: "Put. That coffee. Down. Coffee is for closers only.",  title: "Blake", rank: 5} }
 
   let(:characters) { [boris, charles, wolf, glen] }
 
   it 'test_where_with_exact_match' do
-    expect(characters.where(:name => 'The Wolf')).to eq [wolf]
+    expect(characters.where(name: 'The Wolf')).to eq [wolf]
   end
 
   it 'test_where_with_partial_match' do
-    expect(characters.where(:title => /^B.*/)).to eq [charles, glen]
+    expect(characters.where(title: /^B.*/)).to eq [charles, glen]
   end
 
   it 'test_where_with_mutliple_exact_results' do
-    expect(characters.where(:rank => 4)).to eq [boris, wolf]
+    expect(characters.where(rank: 4)).to eq [boris, wolf]
   end
 
   it 'test_with_with_multiple_criteria' do
-    expect(characters.where(:rank => 4, :quote => /get/)).to eq [wolf]
+    expect(characters.where(rank: 4, quote: /get/)).to eq [wolf]
   end
 
   it 'test_with_chain_calls' do
-    expect(characters.where(:quote => /if/i).where(:rank => 3)).to eq [charles]
+    expect(characters.where(quote: /if/i).where(:rank => 3)).to eq [charles]
+  end
+
+  it 'returns an empty array when no matches' do
+
   end
 
 end
--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -2,23 +2,20 @@
 module ArrayExtension
   module Where
     def where(**search_params)
-      results = []
+      attr, search_val =  search_params.first
+      search_params.delete(attr)
 
-      search_params.each do |search|
-        attr, search_val = search
-
-        found = self.select do |character|
-          if search_val.is_a? Regexp
-            character[attr].match?(search_val)
-          else
-            character[attr] == search_val
-          end
+      matches = self.select do |character|
+        if search_val.is_a? Regexp
+          character[attr].match?(search_val)
+        else
+          character[attr] == search_val
         end
-
-        results += found
       end
 
-      results
+      return matches if search_params.empty? || matches.empty?
+
+      matches.where(search_params)
     end
   end
 end
