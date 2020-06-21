d662d8b6e7e9104500e917b12864e2ac96bafd7c
CargoElf <benjamin.m.vansickle@gmail.com>
2020-06-21 15:26:29 -0500

    reformat hand_spec

--- ruby/blackjack/spec/hand_spec.rb
+++ ruby/blackjack/spec/hand_spec.rb
@@ -21,59 +21,61 @@ RSpec.describe Hand do
     end
   end
 
-  describe '.hand_total' do
-    it 'returns the correct total' do
-      hand.cards = [
-        Card.new(:hearts, :ten, 10),
-        Card.new(:spades, :ten, 10)
-      ]
+  context 'Methods' do
+    describe '.hand_total' do
+      it 'returns the correct total' do
+        hand.cards = [
+          Card.new(:hearts, :ten, 10),
+          Card.new(:spades, :ten, 10)
+        ]
 
-      expect(hand.hand_total).to eq 20
+        expect(hand.hand_total).to eq 20
+      end
+
+      it 'totals aces as 1 if total > 10' do
+        hand.cards = [
+          Card.new(:hearts, :ace, [11, 1]),
+          Card.new(:hearts, :ten, 10),
+          Card.new(:spades, :ten, 10)
+        ]
+
+        expect(hand.hand_total).to eq 21
+      end
+
+      it 'totals aces as 11 if total > 11' do
+        hand.cards = [
+          Card.new(:hearts, :ace, [11, 1]),
+          Card.new(:spades, :one, 1)
+        ]
+
+        expect(hand.hand_total).to eq 12
+      end
+
+      it 'totals all ace hand correctly' do
+        hand.cards = [
+          Card.new(:hearts, :ace, [11, 1]),
+          Card.new(:diamonds, :ace, [11, 1]),
+          Card.new(:spades, :ace, [11, 1]),
+          Card.new(:clubs, :ace, [11, 1])
+        ]
+
+        expect(hand.hand_total).to eq 14
+      end
     end
 
-    it 'totals aces as 1 if total > 10' do
-      hand.cards = [
-        Card.new(:hearts, :ace, [11, 1]),
-        Card.new(:hearts, :ten, 10),
-        Card.new(:spades, :ten, 10)
-      ]
+    describe 'to_s' do
+      it 'lists cards in hand as a string' do
+        hand.cards = [
+          Card.new(:hearts, :ace, [11, 1]),
+          Card.new(:diamonds, :ace, [11, 1]),
+          Card.new(:spades, :ace, [11, 1]),
+          Card.new(:clubs, :ace, [11, 1])
+        ]
 
-      expect(hand.hand_total).to eq 21
-    end
-
-    it 'totals aces as 11 if total > 11' do
-      hand.cards = [
-        Card.new(:hearts, :ace, [11, 1]),
-        Card.new(:spades, :one, 1)
-      ]
-
-      expect(hand.hand_total).to eq 12
-    end
-
-    it 'totals all ace hand correctly' do
-      hand.cards = [
-        Card.new(:hearts, :ace, [11, 1]),
-        Card.new(:diamonds, :ace, [11, 1]),
-        Card.new(:spades, :ace, [11, 1]),
-        Card.new(:clubs, :ace, [11, 1])
-      ]
-
-      expect(hand.hand_total).to eq 14
-    end
-  end
-
-  describe 'to_s' do
-    it 'lists cards in hand as a string' do
-      hand.cards = [
-        Card.new(:hearts, :ace, [11, 1]),
-        Card.new(:diamonds, :ace, [11, 1]),
-        Card.new(:spades, :ace, [11, 1]),
-        Card.new(:clubs, :ace, [11, 1])
-      ]
-
-      expect(hand.to_s).to eq(
-        "\n| ACE of HEARTS |\n| ACE of DIAMONDS |\n| ACE of SPADES |\n| ACE of CLUBS |"
-      )
+        expect(hand.to_s).to eq(
+          "\n| ACE of HEARTS |\n| ACE of DIAMONDS |\n| ACE of SPADES |\n| ACE of CLUBS |"
+        )
+      end
     end
   end
 end
